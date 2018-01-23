5be9401f913cc84ee54c17ff2edc40d2e3a502ec
github username <github email address>
2018-01-23 16:28:34 -0500

    nothing

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,11 +4,12 @@ class Word < ApplicationRecord
 
   def self.five_random_words(language_id, user_id)
     language = Language.find(language_id)
+    words_pool = language.words
     user = User.find(user_id)
     five_words = []
     while five_words.length < 5 do
-      index = (rand * language.words.length).ceil
-      word = Word.find(index)
+      index = (rand * words_pool.length).ceil
+      word = words_pool[index]
       if (!user.learned_words.find_by(word_id: word.id) && !five_words.find{|el| el == word})
         five_words << word
       end
